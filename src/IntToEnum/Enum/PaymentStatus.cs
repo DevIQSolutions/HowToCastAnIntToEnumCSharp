3b55099be2448460f9a45f16d3c1824e355fc309
Eric Fleming <eric-fleming18@hotmail.com>
2016-05-09 17:09:38 -0700

    Commit of Casting an int to an enum code base

---
+++ src/IntToEnum/Enum/PaymentStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace IntToEnum.Enum
+{
+    public enum PaymentStatus
+    {
+        //Remember Enums are 0 indexed
+        Pending,
+        Submitted,
+        Accepted
+    }
+ }
---
+++ src/UnitTests/CastIntToPaymentStatusShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using IntToEnum.Enum;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CastIntToPaymentStatusShould
+    {
+        [Test]
+        public void ReturnSubmittedStatus_WhenCastToEnum_GivenAnIntegerValueOfOne()
+        {
+            var statusAsInt = 1;
+            PaymentStatus resultOfCast = (PaymentStatus) statusAsInt;
+
+            Assert.AreEqual(PaymentStatus.Submitted, resultOfCast);
+        }
+
+        [Test]
+        public void ReturnPaymentStatusType_WhenCastToEnum_GivenInvalidEnumValue()
+        {
+            var statusAsInt = 5;
+            PaymentStatus resultOfCast = (PaymentStatus)statusAsInt;
+
+            Assert.IsInstanceOf<PaymentStatus>(resultOfCast);
+        }
+    }
+}
